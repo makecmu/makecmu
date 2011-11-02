989824b050439a9de77530022c75d5fd74a970bc
Max Hawkins <maxhawkins@gmail.com>
2011-11-02 14:05:08 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Makecmu::Application.config.secret_token = '75ca6ca9a8f5cdae432fc2f4786ed84a2eeac13abc2e0b6ab36bf40f1f16822a6a7a3350a8fece44cc3fd601cd10e933416bffe1a94f935669a41318cc86952e'
