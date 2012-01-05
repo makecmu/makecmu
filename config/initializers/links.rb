327f3689104d6fbbb64aa01ed9db86c12627f032
Max Hawkins <maxhawkins@gmail.com>
2012-01-05 14:46:20 -0600

    Refactor / Update Venn diagram

---
+++ config/initializers/links.rb
@@ -0,0 +1,8 @@
+# Since we re-use these throughout the site,
+# avoid repetition by putting them in an
+# impossible-to-find initializer.
+
+LINKS = {
+	:twitter => "http://www.twitter.com/makecmu",
+	:facebook => "http://www.facebook.com/makecmu"
+}
