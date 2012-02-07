a9468f231b75c4c62ec87e63b4a2d956f16a7633
Max Hawkins <maxhawkins@gmail.com>
2012-02-07 16:19:03 -0500

    Update project creation form

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,16 +1,14 @@
 class Project
   include Mongoid::Document
-
-  key :id, :title
+  include Mongoid::Timestamps
 
   field :title, :type => String
-  field :author, :type => String
   field :description, :type => String
   field :looking_for, :type => Array
 
   belongs_to :user
 
-  validates_presence_of :title, :author, :description, :looking_for, :image
+  validates_presence_of :title, :description, :looking_for
 
   mount_uploader :image, ImageUploader
 
