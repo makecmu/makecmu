c19d894356900d97435d699309aad9af8dd562e8
Max Hawkins <maxhawkins@gmail.com>
2012-02-13 03:23:54 -0500

    Switch to the Amerika bukkit for S3

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -3,7 +3,6 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',       # required
     :aws_access_key_id      => ENV['AWS_ID'],       # required
     :aws_secret_access_key  => ENV['AWS_SECRET'],       # required
-    :region                 => 'eu-west-1'  # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'makecmu'                     # required
 end
